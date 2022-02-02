a30fa3503f2617df717ccf8b9b26164680507c7c
itsiky <itsiky@gmail.com>
2022-02-02 10:49:00 +0200

    .

--- pages/about.js
+++ pages/about.js
@@ -3,21 +3,21 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Link from 'next/link'
 
-export default function Home() {
+export default function About() {
   return (
     <div className="container">
       <Head>
-        <title>Itsik!</title>
+        <title>About</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my apps!" />
+        <Header title="About Us" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing <code>pages/about.js</code>
         </p>
         <p className="additions">
-          this is a new line added by me <addition>pages/test.js</addition>
+          this is a new line added by me <addition>pages/about.js</addition>
         </p>
         <Link href="/">
           <a>Home</a>
